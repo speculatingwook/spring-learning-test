06721af45b9b25f1404b3e4bb2ca916e65fab9cb
Yoon ByungWook <yoonbyungwook@Yoonui-MacBookPro.local>
2024-09-12 17:09:59 +0900

    fix: change paramHeader controller

--- src/main/java/nextstep/helloworld/mvc/mapping/ParamHeaderController.java
+++ src/main/java/nextstep/helloworld/mvc/mapping/ParamHeaderController.java
@@ -5,36 +5,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/param-header/message")
+@RequestMapping("/param-header")
 public class ParamHeaderController {
-
-//    @GetMapping
-//    public ResponseEntity<String> message() {
-//        return ResponseEntity.ok().body("message");
-//    }
+    @GetMapping("/message")
+    public ResponseEntity<String> message() {
+        return ResponseEntity.ok("message");
+    }
 
     /**
      *
      * @param name value = name, required = false(기본 값은 true, 만약 파라미터를 받는 것이 정해지지 않았다면 false 지정해줘야 함)
-     * @param header header도 마찬가지
      * @return
      */
-    @GetMapping
-    public ResponseEntity<String> messageForParam(@RequestParam(value = "name", required = false) String name,
-                                                  @RequestHeader(value = "HEADER", required = false) String header) {
-        if(header != null) {
-            return ResponseEntity.ok().body(header);
-        }
-
-        if(name==null && header==null) {
-            return ResponseEntity.ok().body("message");
-        }
-
-        return ResponseEntity.ok().body(name);
+    @GetMapping(value = "/message", params = "name")
+    public ResponseEntity<String> messageForParam(@RequestParam("name") String name) {
+        return ResponseEntity.ok(name);
     }
-//
-//    @GetMapping
-//    public ResponseEntity<String> messageForHeader(@RequestHeader(value = "HEADER") String header) {
-//        return ResponseEntity.ok().body(header);
-//    }
+
+    @GetMapping(value = "/message", headers = "HEADER")
+    public ResponseEntity<String> messageForHeader(@RequestHeader("HEADER") String header) {
+        return ResponseEntity.ok(header);
+    }
+
 }
